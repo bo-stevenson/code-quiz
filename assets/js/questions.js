fdb507ab3ffd6e4509477dd4bb55c05ff353cb3a
Bo Stevenson <bostevenson1027@gmail.com>
2020-10-22 00:35:47 -0500

    added css

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,7 +1,7 @@
 /* Pizza Question list*/
 var questions = [
   {
-    prompt: "How many slices of pizza are eaten each second in the U.S",
+    prompt: "How many slices of pizza are eaten each second in the U.S?",
     options: ["150", "250", "350", "450"],
     answer: "350"
   },
@@ -13,13 +13,13 @@ var questions = [
   },
 
   {
-    prompt: "How big is the diameter of the world's largest pizza",
+    prompt: "How big is the diameter of the world's largest pizza?",
     options: ["122 ft", "134 ft", "155 ft", "90 ft"],
     answer: "122 ft"
   },
 
   {
-    prompt: "In 2013, Domino's made DVD's that smelled like pizza",
+    prompt: "In 2013, Domino's made DVD's that smelled like pizza?",
     options: ["true", "false"],
     answer: "true"
   },
